ef02dda696a46d45d7c67f9a091b4f2a98abac70
Anil Kumar Saini <anilkumarsaini@Anils-MacBook-Pro.local>
2024-04-14 00:21:31 +0530

    Added the important list of the javascript programs

---
+++ Program/index.js
@@ -0,0 +1,63 @@
+// IMPORTANT JAVASCRIPT PROGRAMS LIST
+
+// 1. Sort the array elements.
+
+// 2. Check the given number is Prime number or not.
+
+// 3. Write the Fizz - Buzz program.
+
+// 4. Remove duplicates elements from the array.
+
+// 5. Find the Sum of the array elements.
+
+// 6. Find the Sum(2)(3)().
+
+// 7. Write a program for the different types of Star patterns.
+
+// 8. Reverse the array or string elements.
+
+// 9. Find the count of elements occurence.
+
+// 10. Compare two array elements.
+
+// 11. Make the first letter capital of string.
+
+// 12. Check the given number is perfect number or not.
+
+// 13. Find the Factorial the number.
+
+// 14. Write a program for Pallindrome.
+
+// 15. Write a program for the Fibonic series.
+
+// 16. Write a program for the Anagram.
+
+// 17. Write a program for create a four digit OTP.
+
+// 18. Sum of Two numbers in array.
+
+// 19. Convert array into multiple chunks.
+
+// 20. Program for the Sum(1)(2)(3)
+
+// 21. Write a program for the sum of positive and negative numbers.
+
+// 22. Convert Array into Object
+
+// 23. Convert Array into String
+
+// 24. Convert String into String
+
+// 25. Convert Object into String
+
+// 26. Flat the multi-level array into one array
+
+// 27. Reverse the word of the string eg. ANIL  --->  LINA
+
+// 28. Reverse integer with (-)
+
+// 29. Sum of Roman to Integer number.
+
+// 30. Find the Longest prefix.
+
+// 31. Count the vowels in the string.
