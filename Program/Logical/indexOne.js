3208c8892eed828a358a361736b9608374d75385
anilsaini043 <anilsaini043@gmail.com>
2024-05-08 20:39:24 +0530

    Added list of some logical questions

---
+++ Program/Logical/indexOne.js
@@ -0,0 +1,18 @@
+// 1): Find the longest word in the given string
+
+// 2): Write a function to generate a HashTag from given string
+        // ex: anil kumar saini  ---> AnilKumarSaini
+
+// 3): Count occurence of character  --> Ex: HELLO  --> L = 2 times
+// 4): Check triangle types. --> Ex: Equalateral, Isoscale, Scalene
+// 5): Sort an array elements --> Alphabetecally and Numbers also
+// 6): Determine a given string is Palandrome or not. --> Ex: MADAM === MADAM  -> true
+// 7): Find the Max in the given array
+// 8): Find the Factorial finder of the number
+// 9): Calculate the Average of given list of the numbers
+// 10): Check given two arrays are equal or not.
+// 11): Write a function that takes a number as input and returns the sum of its digits.
+// 12): Remove duplicates from an array integer.
+// 13): Count the Vowels in the given list of string.
+// 14): Check base 2 and power 3 = 8 or others.
+// 15): Find the sum of square of each array element.
