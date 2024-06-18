202dd5b87468c85033ca46bb2428a7a377fa0311
ibm.i.saini <anisain@apac.corpdir.net>
2024-06-18 22:20:11 +0530

    Solution 1 to 5 programs

--- Program/Logical/indexOne.js
+++ Program/Logical/indexOne.js
@@ -1,11 +1,108 @@
 // 1): Find the longest word in the given string
 
+/*
+let str = "My name is Anil Kumar Saini working as frontend developer"
+
+let words = str.split(" ");
+let longWord = "";
+
+for (let word of words) {
+  if (word.length > longWord.length) {
+    longWord = word
+  }
+}
+console.log(longWord) 
+*/
+
+
 // 2): Write a function to generate a HashTag from given string
-        // ex: anil kumar saini  ---> AnilKumarSaini
+// ex: anil kumar saini  ---> AnilKumarSaini
+
+/*
+function hashTagGenerator() {
+  let name = "anil kumar saini";
+  let words = name.split(" ");
+  let hashName = "";
+  for (let word of words) {
+    hashName = hashName + word[0].toUpperCase() + word.slice(1, word.length)
+  }
+  return hashName;
+}
+
+console.log(hashTagGenerator()) 
+*/
+
+
 
 // 3): Count occurence of character  --> Ex: HELLO  --> L = 2 times
+
+/*
+let strr = "Anil kumar saini";
+let obj = {};
+
+for(let el of strr.replaceAll(" ", "")) {
+  let text = el.toLowerCase()
+  obj[text] = obj[text] ? obj[text] + 1 : 1
+}
+
+console.log(obj)
+*/
+
+
+
 // 4): Check triangle types. --> Ex: Equalateral, Isoscale, Scalene
+
+/*
+function checkTriangle(side1, side2, side3){
+	if((side1 === side2) && (side1 === side3)){
+  	return "Equalateral Triangle"
+  }else if( (side1 === side2) || (side2 === side3) || (side1 === side3)){
+  	return "Isoscale Triangle"
+  }else{
+  	return "Scalene Triangle"
+  }
+}
+
+console.log(checkTriangle(3,3,3)) 
+*/
+
+
 // 5): Sort an array elements --> Alphabetecally and Numbers also
+
+// Method one:
+/*
+let numArrOne = [8, 5, 6, 4, 7, 0, 1, 4, 3, 5, 2];
+
+function sortNumArr() {
+  let temp;
+  for (i = 0; i <= numArr.length - 1; i++) {
+    for (j = i + 1; j <= numArr.length - 1; j++) {
+      if (numArr[j] < numArr[i]) {
+        temp = numArr[j];
+        numArr[j] = numArr[i];
+        numArr[i] = temp;
+      }
+    }
+  }
+
+  return numArrOne;
+
+}
+
+console.log("Sorted--> ", sortNumArr())
+
+// Method second:
+let numArrTwo = [8, 5, 6, 4, 7, 0, 1, 4, 3, 5, 2];
+
+let sortNum = numArrTwo.sort();
+console.log(sortNum)
+
+// Method three
+let stringsArr = ["a", "A", "b"].sort();
+console.log(stringsArr); 
+
+*/
+
 // 6): Determine a given string is Palandrome or not. --> Ex: MADAM === MADAM  -> true
 // 7): Find the Max in the given array
 // 8): Find the Factorial finder of the number
@@ -18,8 +115,8 @@
 // 15): Find the sum of square of each array element.
 // 16): Find min value from array. Array may have any types of number like +, - or empty array
 // 17): Write a function to convert a string to camel case and snake_case.
-                // Ex:- Anil kumar saini  ---> anilKumarSaini   (camel case)
-                // Ex:- anilKumarSaini  ---> anil_kumar_saini  (snake_case)
+// Ex:- Anil kumar saini  ---> anilKumarSaini   (camel case)
+// Ex:- anilKumarSaini  ---> anil_kumar_saini  (snake_case)
 
 // 18): Write a function to check if a character is uppercase or lowercase.
 // 19): Write a function to check if a given string starts with a specific substring.
@@ -32,4 +129,4 @@
 // 25): Repeat String  -> in js we have repeat method  --> repeatFunc("anil", 3) // anilanilanil
 // 26): Truncate a String  -->  truncFunc("anil kumar saini", 5) // kumar saini
 // 27): write a function to generate an array containing consecutive numbers from a and b inclusive both --> generateNum(0,5) // [0,1,2,3,4,5]  and Do it as well recursion
-   
+
